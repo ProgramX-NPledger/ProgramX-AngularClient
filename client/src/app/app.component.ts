b436a434a01a3b69aef5d297cf01525bb448eeb4
ProgramX-NPledger <nathan@programx.co.uk>
2025-08-16 12:22:00 +0100

    Added health check

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, inject, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
@@ -7,6 +8,36 @@ import { RouterOutlet } from '@angular/router';
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  private httpClient = inject(HttpClient);
+  
+  protected apiHealthCheck: boolean = false;
+  protected apiHealthCheckMessage: string = '';
+
   title = 'client';
+
+
+
+
+  /**
+   *
+   */
+  constructor() {
+  
+  }
+
+  ngOnInit() {
+    this.httpClient.get('http://localhost:7276/api/v1/healthcheck').subscribe({
+      next: response => {
+        this.apiHealthCheck = true;
+        this.apiHealthCheckMessage = 'API is healthy';
+      },
+      error: error => {
+        this.apiHealthCheck = false;
+        this.apiHealthCheckMessage = error ? error.message : 'API is not healthy';
+      }
+    });
+  }
+    
+  
 }
