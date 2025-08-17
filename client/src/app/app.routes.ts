2f3b3f023e257eff9213bef22af0862010268198
ProgramX-NPledger <nathan@programx.co.uk>
2025-08-17 11:45:09 +0100

    Basic routing

--- client/src/app/app.routes.ts
+++ client/src/app/app.routes.ts
@@ -1,3 +1,16 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '', // default
+        component: HomeComponent
+    },
+    {
+        path: '**',
+        component: HomeComponent,
+        data: {
+            error: 404
+        }
+    }
+];
--- client/src/app/layout/nav/nav.component.ts
+++ client/src/app/layout/nav/nav.component.ts
@@ -1,9 +1,11 @@
 import { Component, inject, OnInit } from '@angular/core';
 import { LoginService } from '../../core/services/login-service.service';
+import { RouterLink } from '@angular/router';
+import { RouterLinkActive } from '@angular/router';
 
 @Component({
   selector: 'app-nav',
-  imports: [],
+  imports: [RouterLink, RouterLinkActive],
   templateUrl: './nav.component.html',
   styleUrl: './nav.component.css'
 })
