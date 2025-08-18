674cad33e2cb23550bdd15e345074f0a48ad9b8a
ProgramX-NPledger <nathan@programx.co.uk>
2025-08-18 21:04:12 +0100

    Improving log in/out status experience

--- client/src/app/home/home.component.ts
+++ client/src/app/home/home.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { LoginService } from '../core/services/login-service.service';
 
 @Component({
   selector: 'app-home',
@@ -6,6 +8,16 @@ import { Component } from '@angular/core';
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
+  router = inject(Router)
+  loginService = inject(LoginService);
 
+  ngOnInit() {
+    // Any initialization logic can go here
+    console.log(this.loginService.currentUser());
+  }
+
+  login() {
+    this.router.navigate(['/login']); 
+  }
 }
