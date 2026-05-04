8068b120b5a906a09a3cea4d61d96d7b8cf1a7af
ProgramX-NPledger <nathan@programx.co.uk>
2026-05-04 11:38:03 +0100

    Adding properties to environment.prod file

--- client/src/environments/environment.prod.ts
+++ client/src/environments/environment.prod.ts
@@ -4,6 +4,9 @@ export const environment = {
   // Update the baseUrl to match your API endpoint
   baseUrl: 'https://fa-programx.azurewebsites.net/api/v1',
   azureFunctionsKey: null,
-  azureAvatarImagesStorageAccountUrl: 'https://saprogramx.blob.core.windows.net/avatar-images'
+  azureAvatarImagesStorageAccountUrl: 'https://saprogramx.blob.core.windows.net/avatar-images',
+  gitHubCommitHash: '((GITHUB_COMMIT_HASH))',
+  buildNumber: '((BUILD_NUMBER))',
+  deployedAt: '((DEPLOYED_AT))'
 };
 
